0abb0265f6004d909cb602299c8be8fcddfa60f1
Guillermo Torres <guillermo98td@gmail.com>
2022-08-17 15:23:52 -0300

    Proyecto Finalizado

--- js/app.js
+++ js/app.js
@@ -12,10 +12,24 @@ const formulario = document.querySelector('#nueva-cita');
 
 const contenedorCitas = document.querySelector('#citas');
 
+let editando;
+
 // Clases
 class Citas{
     constructor(){
         this.citas = [];
+    }
+
+    agregarCita(cita){
+        this.citas = [...this.citas, cita];
+    }
+
+    eliminarCita(id){
+        this.citas = this.citas.filter( cita => cita.id !== id);
+    }
+
+    editarCita(citaActualizada){
+        this.citas = this.citas.map( cita => cita.id === citaActualizada.id ? citaActualizada : cita);
     }
 }
 
@@ -42,6 +56,81 @@ class UI{
         setTimeout(()=>{
             divMensaje.remove();
         }, 2500);
+    }
+
+    imprimirCitas({citas}){
+
+        this.limpiarHTML();
+        
+        citas.forEach(cita => {
+            const {mascota, propietario, telefono, fecha, hora, sintomas, id} = cita;
+
+            const divCita = document.createElement('div');
+            divCita.classList.add('cita', 'p-3')
+            divCita.dataset.id = id;
+
+            //Scripting de losn elementos de la cita
+            const mascotaParrafo = document.createElement('h2');
+            mascotaParrafo.classList.add('card-title', 'font-weight-bolder');
+            mascotaParrafo.textContent = mascota;
+
+            const propietarioParrafo = document.createElement('p');
+            propietarioParrafo.innerHTML = `
+                <span class="font-weight-bold">Propietario: </span> ${propietario}`
+
+            const telefonoParrafo = document.createElement('p');
+            telefonoParrafo.innerHTML = `
+            <span class="font-weight-bold">Telefono: </span> ${telefono}`
+
+            const fechaParrafo = document.createElement('p');
+            telefonoParrafo.innerHTML = `
+            <span class="font-weight-bold">Telefono: </span> ${fecha}`
+
+            const horaParrafo = document.createElement('p');
+            horaParrafo.innerHTML = `
+            <span class="font-weight-bold">Hora: </span> ${hora}`
+
+            const sintomasParrafo = document.createElement('p');
+            sintomasParrafo.innerHTML = `
+            <span class="font-weight-bold">Telefono: </span> ${sintomas}`
+
+            // Boton para eliminar estas citas
+            const btnEliminar = document.createElement('button');
+            btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
+            btnEliminar.innerHTML = 'Eliminar X';
+
+            btnEliminar.onclick = () => eliminarCita(id);
+
+            // Boton para editar las citas
+            const  btnEditar = document.createElement('button');
+            btnEditar.classList.add('btn', 'btn-info');
+            btnEditar.innerHTML = `Editar <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+            </svg>`;
+            btnEditar.onclick = () => editarCita(cita);
+
+
+            // Agregar los parrafos al divCita
+            divCita.appendChild(mascotaParrafo);
+            divCita.appendChild(propietarioParrafo);
+            divCita.appendChild(telefonoParrafo);
+            divCita.appendChild(fechaParrafo);
+            divCita.appendChild(horaParrafo);
+            divCita.appendChild(sintomasParrafo);
+            divCita.appendChild(btnEliminar);
+            divCita.appendChild(btnEditar);
+            
+
+            // Agregar las citas al HTML
+            contenedorCitas.appendChild(divCita);
+
+        })
+    } 
+
+    limpiarHTML(){
+        while(contenedorCitas.firstChild){
+            contenedorCitas.removeChild(contenedorCitas.firstChild);
+        }
     }
 }
 
@@ -89,5 +178,86 @@ function nuevaCita(e){
 
         return;
     }
+
+    if(editando === true){
+        ui.imprimirAlerta('La cita se editó correctamente');
+
+        // Pasar el objeto de la cita a edicion
+        administrarCitas.editarCita({...citaObj})
+        
+        // Regresar el texto del boton a su estado general
+        formulario.querySelector('button[type="submit"]').textContent = 'Crear Cita';
+
+        editando = false
+    }else{
+        // Generar un ID unico
+    citaObj.id = Date.now();
+
+    // Creando una nueva cita
+    administrarCitas.agregarCita({...citaObj});
+
+    // Mensaje de exito
+    ui.imprimirAlerta('Cita agregada con exito');
+    }
+
+    
+    
+    // Reiniciar el objeto para la validacion
+    reiniciarObjeto();
+
+    // Reiniciar el formulario
+    formulario.reset();
+
+    // Imprimir la cita en el DOM
+    ui.imprimirCitas(administrarCitas);
 }
  
+
+function reiniciarObjeto(){
+    citaObj.mascota = '';
+    citaObj.propietario = '';
+    citaObj.telefono = '';
+    citaObj.fecha = '';
+    citaObj.hora = '';
+    citaObj.sintomas = '';
+}
+
+function eliminarCita(id){
+    // Eliminar la cita
+    administrarCitas.eliminarCita(id);
+
+    // Muestre mensaje
+    ui.imprimirAlerta('La cita se elimino correctamente');
+
+    // Refrescar la pagina
+    ui.imprimirCitas(administrarCitas);
+    
+}
+
+// Cargar los datos y el modo edicion
+function editarCita (cita){
+    const { mascota, propietario, telefono, fecha, hora, sintomas, id } = cita;
+
+    // Llenar los inputs
+    mascotaInput.value = mascota;
+    propietarioInput.value = propietario;
+    telefonoInput.value = telefono;
+    fechaInput.value = fecha;
+    horaInput.value = hora;
+    sintomasInput.value = sintomas;
+
+    // Llenar los objetos
+    citaObj.mascota = mascota;
+    citaObj.propietario = propietario;
+    citaObj.telefono = telefono;
+    citaObj.fecha = fecha;
+    citaObj.hora = hora;
+    citaObj.sintomas = sintomas;
+    citaObj.id = id;
+
+    // Cambiar el boton de agregar a editar
+    formulario.querySelector('button[type="submit"]').innerHTML = 'Guardar Cambios';
+
+    editando = true;
+
+}
